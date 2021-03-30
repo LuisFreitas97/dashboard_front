db140c42c76ae2cb4c83ee21e723e9e01c23ac99
LuisFreitas97 <lgf1997@outlook.pt>
2021-03-30 21:17:26 +0100

    Map begin.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <router-link to="/" class="navbar-brand">bezKoder</router-link>
-      <div class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link to="/tutorials" class="nav-link">Tutorials</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/add" class="nav-link">Add</router-link>
-        </li>
-      </div>
-    </nav>
-
-    <div class="container mt-3">
-      <router-view />
-    </div>
+    <Map></Map>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    Map
   }
 }
 </script>
@@ -36,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Map.vue
@@ -0,0 +1,50 @@
+<template>
+  <div style="height: 500px; width: 100%">
+    <l-map :zoom="zoom" :center="center">
+      <!-- <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer> -->
+      <!-- <l-marker :lat-lng="marker"></l-marker> -->
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { latLng } from "leaflet";
+import { LMap } from "vue2-leaflet";
+
+export default {
+  name: "Map",
+  props: {},
+  components: {
+    LMap,
+  },
+  data() {
+    return {
+      zoom: 13,
+      center: latLng(47.41322, -1.219482),
+      //   url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      //   attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      //   marker: latLng(47.41322, -1.219482),
+    };
+  },
+  methods: {
+    initMap() {
+      //   L.map("map").setView([37, 7749, -122, 4194], 13);
+      //   L.map("map", {
+      //     center: [51.505, -0.09],
+      //     zoom: 13,
+      //   });
+    },
+  },
+  mounted() {
+    // this.initMap();
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* #map {
+  height: 500px;
+  width: 500px;
+} */
+</style>
