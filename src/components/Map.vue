06b6abf47e14033127aa15dbd4bf41a2624dae04
LuisFreitas97 <lgf1997@outlook.pt>
2021-03-30 21:49:21 +0100

    Mapa a aparecer corretamente

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="height: 500px; width: 100%">
     <l-map :zoom="zoom" :center="center">
-      <!-- <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer> -->
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
       <!-- <l-marker :lat-lng="marker"></l-marker> -->
     </l-map>
   </div>
@@ -9,20 +9,21 @@
 
 <script>
 import { latLng } from "leaflet";
-import { LMap } from "vue2-leaflet";
+import { LMap, LTileLayer } from "vue2-leaflet";
 
 export default {
   name: "Map",
   props: {},
   components: {
     LMap,
+    LTileLayer
   },
   data() {
     return {
       zoom: 13,
       center: latLng(47.41322, -1.219482),
-      //   url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-      //   attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       //   marker: latLng(47.41322, -1.219482),
     };
   },
