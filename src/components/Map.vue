0af3c07df561a01e1f128ea235e39bbc9f016e67
LuisFreitas97 <lgf1997@outlook.pt>
2021-04-02 15:56:06 +0100

    desenhar várias áreas de várias formas no mapa.

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,43 +1,109 @@
 <template>
-  <div style="height: 500px; width: 100%">
+  <div style="height: 1000px; width: 100%">
     <l-map :zoom="zoom" :center="center">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-      <!-- <l-marker :lat-lng="marker"></l-marker> -->
+      <l-marker :lat-lng="marker">
+        <l-icon
+          :icon-size="icon.size"
+          :icon-anchor="icon.anchor"
+          :icon-url="icon.url"
+        />
+      </l-marker>
+      <l-circle :lat-lng="circle.center" :radius="circle.radius" />
+      <l-rectangle :bounds="rectangle.bounds" :l-style="rectangle.style" />
+      <l-polygon :lat-lngs="polygon.latlngs" :color="polygon.color" />
+      <l-polyline :lat-lngs="polyline.latlngs" :color="polyline.color" />
     </l-map>
   </div>
 </template>
 
 <script>
 import { latLng } from "leaflet";
-import { LMap, LTileLayer } from "vue2-leaflet";
+import {
+  LMap,
+  LMarker,
+  LTileLayer,
+  LCircle,
+  LRectangle,
+  LPolygon,
+  LPolyline,
+  LIcon,
+} from "vue2-leaflet";
 
 export default {
   name: "Map",
   props: {},
   components: {
     LMap,
-    LTileLayer
+    LMarker,
+    LTileLayer,
+    LCircle,
+    LRectangle,
+    LPolygon,
+    LPolyline,
+    LIcon,
   },
   data() {
     return {
-      zoom: 13,
-      center: latLng(47.41322, -1.219482),
-        url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-        attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      //   marker: latLng(47.41322, -1.219482),
+      // zoom: 13,
+      // center: latLng(41.8239, -7.79006),
+      icon: {
+        size: [32,37],
+        anchor: [16,37],
+        url: "/images/icon/marker.png"
+      },
+      zoom: 11,
+      center: [47.31322, -1.319482],
+      circle: {
+        center: latLng(47.41322, -1.0482),
+        radius: 4500,
+      },
+      rectangle: {
+        bounds: [
+          [47.341456, -1.397133],
+          [47.303901, -1.243813],
+        ],
+        style: { color: "red", weight: 5 },
+      },
+      polygon: {
+        latlngs: [
+          [47.2263299, -1.6222],
+          [47.21024000000001, -1.6270065],
+          [47.1969447, -1.6136169],
+          [47.18527929999999, -1.6143036],
+          [47.1794457, -1.6098404],
+          [47.1775788, -1.5985107],
+          [47.1676598, -1.5753365],
+          [47.1593731, -1.5521622],
+          [47.1593731, -1.5319061],
+          [47.1722111, -1.5143967],
+          [47.1960115, -1.4841843],
+          [47.2095404, -1.4848709],
+          [47.2291277, -1.4683914],
+          [47.2533687, -1.5116501],
+          [47.2577961, -1.5531921],
+          [47.26828069, -1.5621185],
+          [47.2657179, -1.589241],
+          [47.2589612, -1.6204834],
+          [47.237287, -1.6266632],
+          [47.2263299, -1.6222],
+        ],
+        color: "#ff00ff",
+      },
+      polyline: {
+        latlngs: [
+          [47.334852, -1.509485],
+          [47.342596, -1.328731],
+          [47.241487, -1.190568],
+          [47.234787, -1.358337],
+        ],
+        color: "green",
+      },
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: latLng(47.41322, -1.219482),
     };
-  },
-  methods: {
-    initMap() {
-      //   L.map("map").setView([37, 7749, -122, 4194], 13);
-      //   L.map("map", {
-      //     center: [51.505, -0.09],
-      //     zoom: 13,
-      //   });
-    },
-  },
-  mounted() {
-    // this.initMap();
   },
 };
 </script>
