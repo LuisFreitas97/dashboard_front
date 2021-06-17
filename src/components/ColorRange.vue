2e9c9d7f13932bde4b0ffb8ca258e021662889f2
LuisFreitas97 <lgf1997@outlook.pt>
2021-06-17 18:39:52 +0100

    Componente do range card configurável.

--- src/components/ColorRange.vue
+++ src/components/ColorRange.vue
@@ -1,35 +1,18 @@
 <template>
   <v-card
-    class="mx-auto"
+    class="range-card"
     outlined
-    style="
-      z-index: 500;
-      position: absolute;
-      margin-left: 87% !important;
-      margin-rigth: 5% !important;
-      margin-top: 3% !important;
-    "
   >
-    <v-list-item>
-      <v-list-item-avatar tile size="25" color="#ffcbdb"></v-list-item-avatar>
-      <v-list-item-content> &#62;25&#8451; </v-list-item-content>
-    </v-list-item>
-    <v-list-item>
-      <v-list-item-avatar tile size="25" color="#ffa500"></v-list-item-avatar>
-      <v-list-item-content> 20-25&#8451; </v-list-item-content>
-    </v-list-item>
-    <v-list-item>
-      <v-list-item-avatar tile size="25" color="#ffd153"></v-list-item-avatar>
-      <v-list-item-content> 15-20&#8451; </v-list-item-content>
-    </v-list-item>
-    <v-list-item>
-      <v-list-item-avatar tile size="25" color="#c4d348"></v-list-item-avatar>
-      <v-list-item-content> 10-15&#8451; </v-list-item-content>
-    </v-list-item>
-    <v-list-item>
-      <v-list-item-avatar tile size="25" color="#55cb50"></v-list-item-avatar>
-      <v-list-item-content> &#60;10&#8451; </v-list-item-content>
-    </v-list-item>
+    <div v-for="range in ranges" v-bind:key="range.color">
+      <v-list-item>
+        <v-list-item-avatar
+          tile
+          size="25"
+          :color="range.color"
+        ></v-list-item-avatar>
+        <v-list-item-content> {{ range.text }} &#8451; </v-list-item-content>
+      </v-list-item>
+    </div>
   </v-card>
 </template>
 
@@ -39,7 +22,24 @@ export default {
   props: {},
   components: {},
   data() {
-    return {};
+    return {
+      ranges: [
+        { color: "#ffcbdb", text: ">25" },
+        { color: "#ffa500", text: "20-25" },
+        { color: "#ffd153", text: "15-20" },
+        { color: "#c4d348", text: "10-15" },
+        { color: "#55cb50", text: "<10" },
+      ],
+    };
   },
 };
 </script>
+
+<style scoped>
+.range-card {
+  z-index: 500;
+  position: absolute;
+  margin-left: 87% !important;
+  margin-top: 3% !important;
+}
+</style>
